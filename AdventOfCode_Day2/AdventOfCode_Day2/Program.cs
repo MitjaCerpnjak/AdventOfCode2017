c2ca3458b2b723508179de11a6dc22f531259272
mcerpnjak <mitjacerpnjak@gmail.com>
2017-12-04 13:22:09 +0100

    Update
    Added description, code change

--- AdventOfCode_Day2/AdventOfCode_Day2/Program.cs
+++ AdventOfCode_Day2/AdventOfCode_Day2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +11,36 @@ namespace AdventOfCode_Day2
     {
         static public int AdventOfCode_Part1(string values)
         {
-            List<string> listStrLineElements = values.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).ToList();
-            List<string> rowList = listStrLineElements.SelectMany(s => s.Split('\t')).ToList();
-            List<int> numbers = rowList.Select(s => int.Parse(s)).ToList();
-            int j = 1;
+            /*
+             * --- Day 2: Corruption Checksum ---
 
+            As you walk through the door, a glowing humanoid shape yells in your direction. "You there! Your state appears to be idle. 
+            Come help us repair the corruption in this spreadsheet - if we take another millisecond, we'll have to display an hourglass cursor!"
+
+            The spreadsheet consists of rows of apparently-random numbers. To make sure the recovery process is on the right track, they need you to calculate the spreadsheet's checksum. 
+            For each row, determine the difference between the largest value and the smallest value; the checksum is the sum of all of these differences.
+
+            For example, given the following spreadsheet:
+            5 1 9 5
+            7 5 3
+            2 4 6 8
+
+            The first row's largest and smallest values are 9 and 1, and their difference is 8.
+            The second row's largest and smallest values are 7 and 3, and their difference is 4.
+            The third row's difference is 6.
+            In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
+
+            What is the checksum for the spreadsheet in your puzzle input?
+             */
+
+            // Get data from .txt file
+            List<string> listStrLineElements = values.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+            // Split it by the occurance of TAB(\t)
+            List<string> rowList = listStrLineElements.SelectMany(s => s.Split('\t')).ToList();
+            // Create a List of split values
+            List<int> numbers = rowList.Select(s => int.Parse(s)).ToList();
+
+            int j = 1;
             int min = 0;
             int max = 0;
             int result = 0;
@@ -38,7 +63,6 @@ namespace AdventOfCode_Day2
                     }
                     j++;
                 }
-                //Console.WriteLine("\nMaximum: " + max + "\nMinimum: " + min);
                 result += max - min;
             }
 
@@ -47,8 +71,33 @@ namespace AdventOfCode_Day2
 
         static public int AdventOfCode_Part2(string values)
         {
+            /*
+             * --- Part Two ---
+
+            "Great work; looks like we're on the right track after all. Here's a star for your effort." However, the program seems a little worried. Can programs be worried?
+
+            "Based on what we're seeing, it looks like all the User wanted is some information about the evenly divisible values in the spreadsheet. Unfortunately, none of us are equipped for that kind of calculation - most of us specialize in bitwise operations."
+
+            It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
+
+            For example, given the following spreadsheet:
+
+            5 9 2 8
+            9 4 7 3
+            3 8 6 5
+            In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+            In the second row, the two numbers are 9 and 3; the result is 3.
+            In the third row, the result is 2.
+            In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+            What is the sum of each row's result in your puzzle input?
+            */
+
+            // Get data from .txt file
             List<string> listStrLineElements = values.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+            // Split it by the occurance of TAB(\t)
             List<string> rowList = listStrLineElements.SelectMany(s => s.Split('\t')).ToList();
+            // Create a List of split values
             List<int> numbers = rowList.Select(s => int.Parse(s)).ToList();
 
             int division = 0;
@@ -62,25 +111,29 @@ namespace AdventOfCode_Day2
 
             foreach (int num in numbers)
             {
-                if (count == 16)
+                // Check if all values have been retrieved. If true, end function
+                if (count == (numbers.Count/16))
                 {
                     break;
                 }
                 for (; i < pinPoint; i++)
                 {
                     division = 0;
+                    // Prevents the value to divide itself
                     if (numbers[j] != numbers[i])
                     {
+                        // Check if Number1 evenly divides Number2
                         if (numbers[j] % numbers[i] == 0)
                         {
                             division = numbers[j] / numbers[i];
-                            i = j = pinPoint;
-                            pinPoint += 16;
+                            i = j = pinPoint; // Set the current index to the next row of data
+                            pinPoint += 16; // Increase pinPoint to work on next 16 values instead of the same 
                             result += division;
                             count++;
                             break;
                         }
-                        else if(numbers[i] % numbers[j] == 0)
+                        // Check if Number2 evenly divides Number1
+                        else if (numbers[i] % numbers[j] == 0)
                         {
                             division = numbers[i] / numbers[j];
                             i = j = pinPoint;
@@ -91,9 +144,10 @@ namespace AdventOfCode_Day2
                         }
                     }
                     sum = division;
-                    
-                    //Console.WriteLine("\nNum: " + numbers[j] + "\nArray Number: " + numbers[i] + "\nDivision: " + division + "\nIndex: " + i + "\nPin Point: " + pinPoint + "\nResult: " + result);
 
+                    // If we get to the end of the row and there's no value retrieved,
+                    // set index at the beginning of current row and move to the next
+                    // value to compare
                     if (i == pinPoint - 1)
                     {
                         if (sum == 0)
@@ -110,15 +164,14 @@ namespace AdventOfCode_Day2
         }
         static void Main(string[] args)
         {
+            // Get path to executable file (\bin\Debug)
             string path = AppDomain.CurrentDomain.BaseDirectory;
-            string input = File.ReadAllText(Path.GetFullPath(Path.Combine(path, @"..\..\input.txt")));
+            // Read all data in file + go back 2 directories 
+            // (had to do this since I delete \bin + \obj folders to ge the whole project uploaded)
+            string input = File.ReadAllText(Path.GetFullPath(Path.Combine(path, @"..\..\input.txt"))); 
             
-            int partOne = AdventOfCode_Part1(input);
-            Console.WriteLine("Advent of Code Day 2; Part One\nSum: " + partOne);
-            
-            int partTwo = AdventOfCode_Part2(input);
-            Console.WriteLine("\n\nAdvent of Code Day 2; Part Two\nSum: " + partTwo);
-
+            Console.WriteLine("Advent of Code Day 2; Part One\nSum: " + AdventOfCode_Part1(input));
+            Console.WriteLine("\n\nAdvent of Code Day 2; Part Two\nSum: " + AdventOfCode_Part2(input));
         }
     }
 }
